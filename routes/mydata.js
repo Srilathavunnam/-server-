36967952527551d62f368f97c8c25599d6910119
Srilathavunnam <89418789+Srilathavunnam@users.noreply.github.com>
2021-10-27 00:27:49 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Hema Sesha Sai Pamulapati' });
+  res.render('mydata', { title: 'Srilatha Vunnam' });
 });
 
 module.exports = router;
