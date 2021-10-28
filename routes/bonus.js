f7be76ec572939b92581347533d0cfc591ebfcc5
Srilathavunnam <89418789+Srilathavunnam@users.noreply.github.com>
2021-10-27 22:53:59 -0500

    new endpoint

--- routes/bonus.js
+++ routes/bonus.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var rand=Math.floor(Math.random());
-var value=Math.cos(1);
+var rand=Math.random();
+var value=Math.cos(rand);
+
 
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-  res.send('[Math.cos()] applied to ['+1+'] is ['+value+']');
+  res.send('[Math.cos()],[Math.asin()] applied to ['+rand+'] is ['+value+'],['+Math.asin(rand)+'],'+'['+Math.asinh(rand)+']');
 });
 
 module.exports = router;
